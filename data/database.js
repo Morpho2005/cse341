cfd46ce310394d1b419efaad5145288dcca13d82
morpho2005 <digitalchristian05@gmail.com>
2025-05-09 17:24:05 -0600

    commit

--- data/database.js
+++ data/database.js
@@ -22,7 +22,7 @@ const initDb = (callback) => {
 
 const getDatabase = () => {
     if (!database){
-        throw Error
+        throw Error('Database not initialized')
     }
     return database;
 }
